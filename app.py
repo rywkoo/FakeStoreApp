8cc8321c7678abae58c771608471e16d609e704b
Pinchai <>
2025-07-08 13:16:52 +0700

    add template

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
-import  requests
+import requests
 
 app = Flask(__name__)
 
+# @app.get('/')
+# @app.get('/home')
+# def home():
+#     return render_template('master.html')
+#
+#
 
 @app.get('/')
 @app.get('/home')
@@ -18,28 +24,9 @@ def home():
     return render_template('home.html', products=products, error=error)
 
 
-@app.get('/about')
+@app.get('/support')
 def about():
-    return render_template('about.html')
-
-
-@app.get('/api/products')
-def getProducts():
-    products = [
-        {
-            "id": 1,
-            "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-            "price": 109.95,
-            "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-            "category": "men's clothing",
-            "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-            "rating": {
-                "rate": 3.9,
-                "count": 120
-            }
-        },
-    ]
-    return products
+    return render_template('support.html')
 
 
 if __name__ == '__main__':
